561c38fd81869edffa0c174b7ee5f942e962fac8
Mitch-Coder <michellescalderon2002@gmail.com>
2024-05-26 17:11:10 -0600

    Info Employees

--- src/app/components/employee/employee.module.ts
+++ src/app/components/employee/employee.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { DialogService } from 'src/app/modules/dialog/service/dialog.service';
 import { MatDialogModule } from '@angular/material/dialog';
+import { RollModule } from './roll/roll.module';
+import { ProfileModule } from './profile/profile.module';
+import { ViewDataModule } from 'src/app/modules/view-data/view-data.module';
 import { EmployeeModule1 } from './employee/employeeInfo.module';
-
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     EmployeeRoutingModule,
     MatDialogModule,
+    RollModule, 
+    ProfileModule,
+    ViewDataModule,
     EmployeeModule1
   ],
   providers: [DialogService],
