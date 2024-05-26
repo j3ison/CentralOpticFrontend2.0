61bb653d4d5471c70352cf7ba223725c6b0fcba0
Mitch-Coder <michellescalderon2002@gmail.com>
2024-05-26 12:18:17 -0600

    employee changess

---
+++ src/app/components/employee/employee/employeeInfo.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeComponent } from './employee.component';
+import { TableModule } from "../../../modules/table/table.module";
+import { ViewDataModule } from 'src/app/modules/view-data/view-data.module';
+import { MatInputModule } from '@angular/material/input';
+
+
+@NgModule({
+    declarations: [
+        EmployeeComponent
+    ],
+    imports: [
+        CommonModule,
+        TableModule,
+        ViewDataModule,
+        MatInputModule,
+    ],
+    exports:[EmployeeComponent]
+})
+export class EmployeeInfoModule { }
