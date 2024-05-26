cccb5cd071e40ec90a67600637c6470db448f6b0
Mitch-Coder <michellescalderon2002@gmail.com>
2024-05-26 15:03:20 -0600

    test

--- src/app/components/employee/employee.module.ts
+++ src/app/components/employee/employee.module.ts
@@ -3,18 +3,16 @@ import { CommonModule } from '@angular/common';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { DialogService } from 'src/app/modules/dialog/service/dialog.service';
 import { MatDialogModule } from '@angular/material/dialog';
-import { EmployeeModule1 } from './employee/employee.module';
-import { RollModule } from './roll/roll.module';
-import { ProfileModule } from './profile/profile.module';
+import { EmployeeModule1 } from './employee/employeeInfo.module';
+
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     EmployeeRoutingModule,
     MatDialogModule,
-    EmployeeModule1,
-    RollModule, 
-    ProfileModule
+    EmployeeModule1
   ],
   providers: [DialogService],
 })
