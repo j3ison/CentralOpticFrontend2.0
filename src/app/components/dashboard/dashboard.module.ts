e73f1023e2f34a6af7f47abd3e21673d33f5a19e
Jurgen hace programas. <jurgenfbermudezpicado@gmail.com>
2024-06-27 14:15:41 -0600

    Agregando estadisticas sin graficos

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -5,16 +5,22 @@ import {  MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ViewDataModule } from 'src/app/modules/view-data/view-data.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
+    MatInputModule,
+    MatFormFieldModule,
     CommonModule,
     MatCardModule,
     MatIconModule,
     MatButtonModule,
+    MatDatepickerModule,
     ViewDataModule
   ], 
   exports:[DashboardComponent]
